ffd2d8b8b3a69ff861e3503749def174b5db5708
pitch <95540893+pitch2@users.noreply.github.com>
2024-09-09 16:30:08 +0200

    Add files via upload

---
+++ InstallAgentZabbix2WithChocoV3.ps1
@@ -0,0 +1,54 @@
+﻿$PClisting = [System.Collections.ArrayList]@('')
+$cible_txt = Get-Content xxx\cible.txt
+$PClisting.Add($cible_txt)
+
+if ($PClisting -ne $null)
+{
+    foreach ($item in $PClisting)
+    {
+        try
+        {
+        # Create new remote PS session object
+        $s= New-PSSession -ComputerName $item 
+        $packageName="zabbix-agent2"
+
+        Invoke-Command -Session $s -ScriptBlock { 
+       
+    
+        function SetParamsMSI_AgentZabbix2 {
+	        param(
+		        [Parameter(Mandatory=$true, Position=0)][string] $installFolder,
+		        [Parameter(Mandatory=$true, Position=1)][string] $hostname
+	        )
+           
+                $a="/LOGTYPE:file /LOGFILE:"+ "'" + $installFolder + "\zabbix_agentd.log" + "'" + " /SERVER:xxxx /LISTENPORT:xxxx /SERVERACTIVE:xxxx /INSTALLFOLDER:"+ "'" + $installFolder + "'" +" /HOSTNAME:"+ "'" + $hostname + "'" + " /TLSCONNECT:psk /TLSACCEPT:psk /TLSPSKIDENTITY:xxxxx /TLSPSKVALUE:xxxxx /ALLOWDENYKEY:AllowKey=system.run[*] /SKIP:fw" #change the XXXX
+                $ParamsList='--package-parameters='+$a
+                
+                $o=$ParamsList 
+            return $o
+        }
+            
+        if (-not (Test-Path -LiteralPath “C:\Program Files\Zabbix Agent")) 
+        {
+            choco feature enable -n=allowGlobalConfirmation
+            
+            $packageArgs=SetParamsMSI_AgentZabbix2 “C:\Program Files\Zabbix Agent" $using:item 
+
+            choco install $using:packageName $packageArgs
+    
+            $exitCode = $LASTEXITCODE
+            choco feature disable -n=allowGlobalConfirmation    
+        }
+        }    
+        
+        # Clean session disconnection
+        Disconnect-PSSession -Session $s 
+
+        Write-Host "Successfully deployed package: " $packageName + " on: " $item  -ForegroundColor Cyan
+        }
+        catch{
+            $Error[0].Exception.Message
+            Write-Host "Something was getting wrong with: " $exitCode + " on: " $item  -ForegroundColor Red
+        } 
+    }
+}
