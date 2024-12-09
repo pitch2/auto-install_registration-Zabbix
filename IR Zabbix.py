75c6c0e41f927e79bb1d636495a3eb516d9f68bf
pitch <95540893+pitch2@users.noreply.github.com>
2024-12-09 11:22:46 +0100

    Update IR Zabbix.py

--- IR Zabbix.py
+++ IR Zabbix.py
@@ -1,26 +1,27 @@
 import subprocess, sys, csv
 from zabbix_api import ZabbixAPI
+from datetime import datetime
 
 #establish connection with Zabbix server 
-zabbix_url = 'http://xxxx/zabbix/api_jsonrpc.php' # Your server and /api_jsonrpc.php
-zabbix_user = 'xxxx'  # Your user
-zabbix_password = 'xxxx'  # password of user
+zabbix_url = 'http://<IPSERVER>/zabbix/api_jsonrpc.php' # Your server and /api_jsonrpc.php
+zabbix_user = '<USER>'  # Your user
+zabbix_password = '<PWD>'  # password of user
 #connect
 zapi = ZabbixAPI(server=zabbix_url)
 zapi.login(zabbix_user, zabbix_password)
 
 #Function using API for add host
-def send_API(host_name, host_ip, group_id, template_id, n_DNS):
+def send_API(host_name, host_ip, group_id, nom_DNS):
     try:
         host = zapi.host.create({
             'host': host_name,
             'interfaces': [{
                 'type': 1,  # Type d'interface (1 = agent Zabbix)
                 'main': 1,  # Interface principale
-                'useip': 0,  # Utiliser le DNS (0 = utiliser DNS, 1 = utiliser IP)
+                'useip': 1,  # Utiliser le DNS (0 = utiliser DNS, 1 = utiliser IP)
                 'ip': host_ip,  # Adresse IP de l'hôte
-                'dns': n_DNS,  # Nom DNS de l'hôte
-                'port': 'xxxx',  # Port par défaut pour l'agent Zabbix
+                'dns': nom_DNS,  # Nom DNS de l'hôte
+                'port': '<PORT>',  # Port par défaut pour l'agent Zabbix
                 }],
             'groups': [{'groupid': group_id}],
             'templates': [{'templateid': template_id}],
@@ -31,64 +32,22 @@ def send_API(host_name, host_ip, group_id, template_id, n_DNS):
 
 
 
-
 # Read the docs for to find your ID
-group_id = '23'  # ID host group
-template_id = '10637'  # ID of template 
+group_id = '<GROUPID>'  # ID host group
+template_id = '<TEMPLATEID>'  # ID of template 
 
-
-
-with open(r'xxxx\liste.csv', newline='') as csv_file:
-    reader = csv.reader(csv_file, delimiter=',')
+with open(r'liste.csv', newline='') as csv_file:
+    reader = csv.reader(csv_file, delimiter=';')
     rows = list(reader)
 
-    for i in range(len(rows)):
-
-        name = rows[i][0].split(";")[1].split(".")[0]
-        ip = rows[i][0].split(";")[0]
-        n_i = rows[i][0].split(";")[1].split(".")[0], rows[i][0].split(";")[0]
-
-        f = open("cible.txt", "w")
-        f.write(name)
-        f.close
-
-        f = open("logs.txt", "a")
-        f.write(f"Start install of {n_i}\n")
-        f.close
-
-        #install Zabbix agent with powershell script
-        p = subprocess.Popen(["powershell.exe", r"xxxx\InstallAgentZabbix2WithChocoV3.ps1"], stdout=sys.stdout) #location of PS1 script
-        p.wait()
-
-        f = open("logs.txt", "a")
-        f.write("   Install is good\n")
-        f.close
-
-        send_API(rows[i][0].split(";")[1].split(".")[0], rows[i][0].split(";")[0], group_id, template_id, rows[i][0].split(";"))
-
-        f = open("logs.txt", "a")
-        f.write("   Registre in Zabbix\n")
-        f.close
-
-        f = open("logs.txt", "a")
-        f.write(f"{n_i} is finish\n")
-        f.write("\n\n\n")
-        f.close
-
-        print(f"{n_i} ,is finish")
-    
-    f = open("logs.txt", "a")
-    f.write(f"----------------------------------FINISH SESSION------------------------------------\n\n\n\n")
-    f.close
-
-
-
-
-
-
-
-
-
-
+    for i in range(len(rows[0])):
+        host = rows[0][i].split(",")
         
-
+        # Check if the split resulted in at least 2 elements
+        if len(host) >= 2:
+            name = host[0]
+            ip = host[1]
+            print(name, ip)
+            
+        send_API(name ,ip, group_id, name)
+        print("OK",name, ip)
