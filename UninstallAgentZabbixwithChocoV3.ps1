7cae0a6ed1fb8d76c93bb490a97942357e3f47ab
pitch <95540893+pitch2@users.noreply.github.com>
2024-12-18 14:46:06 +0100

    Update UninstallAgentZabbixwithChocoV3.ps1

--- UninstallAgentZabbixwithChocoV3.ps1
+++ UninstallAgentZabbixwithChocoV3.ps1
@@ -1,6 +1,5 @@
 $PClisting = [System.Collections.ArrayList]@()
 
-
 if ($PClisting -ne $null)
 {
     foreach ($RemoteComputer in $PClisting)
@@ -15,27 +14,32 @@ if ($PClisting -ne $null)
                 # Check if choco is installed
                 $choco_install_ON = Test-Path "C:\ProgramData\chocolatey" #Verification d'installation de choco
                 $zabbix_install_ON = Test-Path "C:\Program Files\Zabbix Agent" #Verification d'installation de Zabbix
-                $smartmontools_install_ON = Test-Path "C:\Program Files\smartmontools" #Verification d'installation de smartmontools
+                $smartmontools_install_ON = Test-Path "C:\Program Files\smartmontools" #Verification d'installation de 
 
-                if (-not $choco_install_ON) { #si choco n'est installé -> installation
+si choco n'est installé -> installation + si choco installé -> upgrade
+                if (-not $choco_install_ON) {
                     Write-Host "Installing choco on $RemoteComputer" -ForegroundColor Yellow
                     Set-ExecutionPolicy Bypass -Scope Process -Force
                     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-                } else { #si choco est installé -> upgrade
+                } else {
                     Write-Host "Upgrading choco on $RemoteComputer" -ForegroundColor Yellow
                     choco upgrade chocolatey
                 }
-                if ($smartmontools_install_ON) { #si smart est installé -> désinstallation
-                    Write-Host "Uninstalling smartmontools on $RemoteComputer" -ForegroundColor Yellow
+
+#si smart est installé -> désinstallation
+                if ($smartmontools_install_ON) {
+                    Write-Host "Uninstalling smartmontools on" $RemoteComputer -ForegroundColor Yellow
                     choco uninstall smartmontools -y --removedependencies
                 } else {
-                    Write-Host "No smartmontools in $RemoteComputer" -ForegroundColor Cyan
+                    Write-Host "No smartmontools on" $RemoteComputer -ForegroundColor Cyan
                 }
-                if ($zabbix_install_ON) { #si zabbix est installé -> désinstallation
-                    Write-Host "Uninstalling Zabbix Agent on $RemoteComputer" -ForegroundColor Yellow
+
+#si zabbix est installé -> désinstallation
+                if ($zabbix_install_ON) {
+                    Write-Host "Uninstalling Zabbix Agent on" $RemoteComputer -ForegroundColor Yellow
                     choco uninstall zabbix-agent2 -y --removedependencies
                 } else {
-                    Write-Host "No zabbix agent on $RemoteComputer" -ForegroundColor Cyan
+                    Write-Host "No zabbix agent on" $RemoteComputer -ForegroundColor Cyan
                 }
             }
 
