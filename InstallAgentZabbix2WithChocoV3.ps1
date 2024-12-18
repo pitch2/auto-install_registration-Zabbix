bab32672690f147fd45846228d1cc5d51389716e
pitch <95540893+pitch2@users.noreply.github.com>
2024-12-18 14:46:33 +0100

    Update InstallAgentZabbix2WithChocoV3.ps1

--- InstallAgentZabbix2WithChocoV3.ps1
+++ InstallAgentZabbix2WithChocoV3.ps1
@@ -10,19 +10,26 @@ if ($PClisting -ne $null)
             try
             {
                 Invoke-Command -ComputerName $RemoteComputer -ScriptBlock {
+
                     Set-ExecutionPolicy Unrestricted
                     choco feature enable -n=allowGlobalConfirmation
-                    choco install smartmontools
-                    #choco install zabbix-agent2 --package-parameters "/LOGTYPE:file /LOGFILE: C:\Program Files\Zabbix Agent  \zabbix_agentd.log /SERVER:<IPSERVER> /LISTENPORT:<PORT> /SERVERACTIVE:<IPSERVERORPROXY> /INSTALLFOLDER: C:\Program Files\Zabbix Agent /HOSTAME: $RemoteComputer /TLSCONNECT:psk /TLSACCEPT:psk /TLSPSKIDENTITY:<PSKNAME> /TLSPSKVALUE:<PSKKEY> /ALLOWDENYKEY:AllowKey=system.run[*]"
-                    choco install zabbix-agent2 --package-parameters "/LOGTYPE:file /LOGFILE: C:\Program Files\Zabbix Agent  \zabbix_agentd.log /SERVER:<IPSERVER> /LISTENPORT:<PORT> /SERVERACTIVE:<IPSERVERORPROXY> /INSTALLFOLDER: C:\Program Files\Zabbix Agent /HOSTAME: $RemoteComputer"
-                    Set-Content -Path 'C:\Program Files\Zabbix Agent\zabbix_agent2.d\plugins.d\smart.conf' -Value ((Get-Content -Path 'C:\Program Files\Zabbix Agent\zabbix_agent2.d\plugins.d\smart.conf') -replace '# Plugins.Smart.Path=','Plugins.Smart.Path=C:\Program Files\smartmontools\bin\smartctl.exe')
+
+                    choco install smartmontools #Installation smartmontools
+                    Write-Host "Smart is install on" $RemoteComputer-ForegroundColor Yellow
+
+                    choco install zabbix-agent2 --no-progress --package-parameters "/LOGTYPE:file /LOGFILE: C:\Program Files\Zabbix Agent  \zabbix_agentd.log /SERVER:<IPSERVER> /LISTENPORT:<PORT> /SERVERACTIVE:<IPSERVER> /INSTALLFOLDER: C:\Program Files\Zabbix Agent /HOSTAME: $RemoteComputer /TLSCONNECT:psk /TLSACCEPT:psk /TLSPSKIDENTITY:<PSK_ID> /TLSPSKVALUE:<PSK_KEY> /ALLOWDENYKEY:AllowKey=system.run[*]"
+                    Write-Host "Zabbix is install on" $RemoteComputer-ForegroundColor Yellow
+
+                    #FOR NO PSK : choco install zabbix-agent2 --no-progress --package-parameters "/LOGTYPE:file /LOGFILE: C:\Program Files\Zabbix Agent  \zabbix_agentd.log /SERVER:<IPSERVER> /LISTENPORT:<PORT> /SERVERACTIVE:<IPSERVER> /INSTALLFOLDER: C:\Program Files\Zabbix Agent /HOSTAME: $RemoteComputer
+                    Set-Content -Path 'C:\Program Files\Zabbix Agent\zabbix_agent2.d\plugins.d\smart.conf' -Value ((Get-Content -Path 'C:\Program Files\Zabbix Agent\zabbix_agent2.d\plugins.d\smart.conf') -replace '# Plugins.Smart.Path=','Plugins.Smart.Path=C:\Program Files\smartmontools\bin\smartctl.exe') #changement de conf du fichier zabbix -> smart
+
+                    Write-Host "Task successfully completed on: " $RemoteComputer -ForegroundColor Green
                 }
             }
             catch
             {
                 Write-Host "Something was getting wrong: " $RemoteComputer-ForegroundColor Red
             }
-            Write-Host "Task successfully completed on: " $RemoteComputer -ForegroundColor Green
         }
         else
         {
